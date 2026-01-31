53936f7677fe8fbb2652d04c583466b804be3f39
Vikas-kuppasta <vikaskuppasta@gmail.com>
2026-01-31 22:36:57 +0530

    added updated file

--- Frontend/HuntX/src/assets/assets.js
+++ Frontend/HuntX/src/assets/assets.js
@@ -1,3 +1,7 @@
+import { FaArrowUpRightDots } from "react-icons/fa6";
+import { RiBookShelfLine } from "react-icons/ri";
+import { TbHandClick } from "react-icons/tb";
+import { BsStars } from "react-icons/bs";
 const random = (min, max) => {
   return Math.random() * (max - min) + min
 }
@@ -31,4 +35,35 @@ export const animatedCards = [
   { id: 13, title: "Business Analyst", float: createFloat() },
   { id: 14, title: "Sales Executive", float: createFloat() },
   { id: 15, title: "Operations Manager", float: createFloat() },
+]
+export const featuredCards=[
+  {
+    id:1,
+    title:"Got talent?",
+    subtitle:"Why job seekers love us",
+    title1:"Connect directly with founders at top startups - no third party recruiters allowed.",
+    title2:"Everything you need to know, all upfront. View salary, stock options, and more before applying.",
+    title3:"Say goodbye to cover letters - your profile is all you need. One click to apply and you're done.",
+    title4:"Unique jobs at startups and tech companies you can’t find anywhere else.",
+    image1:FaArrowUpRightDots,
+    image2:RiBookShelfLine,
+    image3:TbHandClick,
+    image4:BsStars,
+    bgcolor:"bg-white",
+  },
+  {
+    id:2,
+    title:"Need talent?",
+    subtitle:"Why recruiters love us",
+    title1:"Tap into a community of 10M+ engaged, startup-ready candidates.",
+    title2:"Everything you need to kickstart your recruiting — set up job posts, company branding, and HR tools within 10 minutes, all for free.",
+    title3:"A free applicant tracking system, or free integration with any ATS you may already use.",
+    title4:"Let us handle the heavy-lifting with RecruiterCloud. Our new AI-Recruiter scans 500M+ candidates, filters it down based on your unique calibration, and schedules your favorites on your calendar in a matter of days.",
+    image1:FaArrowUpRightDots,
+    image2:RiBookShelfLine,
+    image3:TbHandClick,
+    image4:BsStars,
+    bgcolor:"bg-blue-100",
+  },
+
 ]
