af599f97a4e6df606ec098c3cc488765d8d8bdeb
Vikas-kuppasta <vikaskuppasta@gmail.com>
2026-06-05 23:20:36 +0530

    resolvetwo

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,9 @@ dotenv.config({});
 const app = express();
 const PORT =process.env.PORT ||5000;
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:[
+        'http://localhost:5173',
+        "https://hiredly-ten.vercel.app"],
     credentials:true,
 }
 
